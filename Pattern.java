c3c2e56920e982ab7dfa32d4240283935ec502d7
pranjal mishra <pranjal.mishra.21cse@bmu.edu.in>
2024-06-30 23:44:43 +0530

    change the ch to try the resolve merge conflict

--- Pattern.java
+++ Pattern.java
@@ -1,7 +1,7 @@
 public class Pattern {
     public static void main(String[] args) {
         int a =12;
-        char ch ='B';
+        char ch ='A';
         System.out.println("new line added for experiment of the pull request in git");
         System.out.println("second line is added to the f1 branch of the git ");
         for(int i=1;i<=a;i++){
