928e241ab6943d17dd9caf4cdb595b0d71cf4d1c
pranjal mishra <pranjal.mishra.21cse@bmu.edu.in>
2024-06-30 08:14:48 +0530

    new branch new change to learn merging

--- Pattern.java
+++ Pattern.java
@@ -2,6 +2,7 @@ public class Pattern {
     public static void main(String[] args) {
         int a =12;
         char ch ='A';
+        System.out.println("added a new line to learn merging in git");
         for(int i=1;i<=a;i++){
             for(int j=1;j<=i;j++){
                 System.out.print(ch);
