bdd7a16858c60179a53692f507724f5b267daeb0
pranjal mishra <pranjal.mishra.21cse@bmu.edu.in>
2024-06-30 07:49:18 +0530

    added few changes

--- Pattern.java
+++ Pattern.java
@@ -1,6 +1,6 @@
 public class Pattern {
     public static void main(String[] args) {
-        int a =5;
+        int a =12;
         char ch ='A';
         for(int i=1;i<=a;i++){
             for(int j=1;j<=i;j++){
